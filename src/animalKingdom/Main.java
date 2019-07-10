478b21e37117d8b585e18e43e477a792580e3179
Devin Warrick <dev.warr@gmail.com>
2019-07-10 14:48:16 -0500

    All animals created and inside arrayList.

--- src/animalKingdom/Main.java
+++ src/animalKingdom/Main.java
@@ -1,9 +1,33 @@
 package animalKingdom;
 
+import java.util.*;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        
+        // Create new ArrayList
+        ArrayList<Animal> animalList = new ArrayList<>();
+
+        // Add in Mammals as they are created
+        animalList.add(new Mammal("Panda", 1869));
+        animalList.add(new Mammal("Zebra", 1778));
+        animalList.add(new Mammal("Koala", 1816));
+        animalList.add(new Mammal("Sloth", 1804));
+        animalList.add(new Mammal("Armadillo", 1758));
+        animalList.add(new Mammal("Raccoon", 1758));
+        animalList.add(new Mammal("Bigfoot", 2021));
+
+        // Add in Birds as they are created
+        animalList.add(new Bird("Pigeon", 1837));
+        animalList.add(new Bird("Peacock", 1821));
+        animalList.add(new Bird("Toucan", 1758));
+        animalList.add(new Bird("Parrot", 1824));
+        animalList.add(new Bird("Swan", 1758));
+
+        // Add in Fish as they are created
+        animalList.add(new Fish("Salmon", 1758));
+        animalList.add(new Fish("Catfish", 1817));
+        animalList.add(new Fish("Perch", 1758));
     }
 }
