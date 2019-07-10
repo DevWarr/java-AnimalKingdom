095b94041ba32c8a94e669cbca2ddb53bb399202
Devin Warrick <dev.warr@gmail.com>
2019-07-10 14:32:46 -0500

    abstract Animal class created.

--- src/animalKingdom/Animal.java
+++ src/animalKingdom/Animal.java
@@ -1,6 +1,27 @@
 package animalKingdom;
 
-public class Animal
+public abstract class Animal
 {
+    private static int maxId = 0;
+    private int id;
+    private String name;
+    private int year;
 
+    public Animal(String name, int year)
+    {
+        maxId++;
+        this.id = maxId;
+        this.name = name;
+        this.year = year;
+    }
+
+    public abstract String move();
+    public abstract String breathe();
+    public abstract String reproduce();
+
+    @Override
+    public String toString()
+    {
+        return "The " + name + " was named in " + year + ".\nIt can " + move() + ", breathe with " + breathe() + ", and reproduces with " + reproduce() + ".\n";
+    }
 }
